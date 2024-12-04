dda4f3b587ec96b8b7155c49592d0c8a47a20b5b
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-12-03 23:12:30 -0400

    Added d and i specifiers to the code

--- _printf.c
+++ _printf.c
@@ -2,6 +2,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Function prototype for print_number */
+int print_number(int n);
+
 /**
  * _printf - Produces output according to a format
  * @format: The format string
@@ -35,12 +38,17 @@ int _printf(const char *format, ...)
 				char *str = va_arg(args, char *);
 
 				if (!str)
-                    str = "(null)";
+					str = "(null)";
 				for (i = 0; str[i]; i++)
 				{
 					_putchar(str[i]);
 					count++;
 				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				count += print_number(num);
 			}
 			else if (*format == '%')
 			{
@@ -63,5 +71,36 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
+	return (count);
+}
+
+/**
+ * print_number - Prints an integer character by character
+ * @n: The integer to print
+ *
+ * Return: The number of characters printed
+ */
+int print_number(int n)
+{
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		count += print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+	count++;
+
 	return (count);
 }
